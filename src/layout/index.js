e9a6d87f49c50f25b817ed3788ef4833adb54cfa
Howard Shelburne <howard.shelburne@gmail.com>
2018-11-26 12:25:36 -0500

    transfered css files

--- src/layout/index.js
+++ src/layout/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-import './layout.css'
+import Sidebar from './sidebar'
+import '../_styles/index.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,7 +18,7 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <React.Fragment>
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -26,20 +26,19 @@ const Layout = ({ children }) => (
             { name: 'keywords', content: 'sample, something' },
           ]}
         >
+          <body className="container" />
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
+        {/*<Header />*/}
+        <div className="row">
+          <main className="col-md-8 col-lg-9">
+            {children}
+          </main>
+          <aside className="col-md-4 col-lg-3 side-bar">
+            <Sidebar />
+          </aside>
         </div>
-      </>
+      </React.Fragment>
     )}
   />
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import ArticleList from '../components/article-list'
-import Sidebar from '../components/sidebar'
-import Layout from '../components/layout'
+import Layout from '../layout'
 
 const ARTICLES = []
 
 const IndexPage = () => (
-  <Layout>
-	  <div className="col-md-8 col-lg-9">
-	    <ArticleList articles={ARTICLES} />
-	  </div>
-	  <div>
-	    <aside className="col-md-4 col-lg-3 side-bar">
-	      <Sidebar />
-	    </aside>
-	  </div>
-  </Layout>
+	<Layout>
+		<ArticleList articles={ARTICLES} />
+	</Layout>
 )
 
 export default IndexPage
