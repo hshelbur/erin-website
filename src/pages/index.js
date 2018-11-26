80b577f446a8583a4a5ad2c90849efffe4159e3d
Howard Shelburne <howard.shelburne@gmail.com>
2018-11-26 15:04:47 -0500

    graphql query for index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,54 @@
 import React from 'react'
-import ArticleList from '../components/article-list'
+import ArticlePreview from '../components/article-preview';
 import Layout from '../layout'
+import {graphql} from 'gatsby'
 
-const ARTICLES = []
-
-const IndexPage = () => (
+const IndexPage = ({data}) => (
 	<Layout>
-		<ArticleList articles={ARTICLES} />
+		<ul className="article-page">
+      
+	        {data.articles.edges.map(({node: article}) => 
+	        	<ArticlePreview 
+		          	key={article.id}
+					title={article.title}
+					date={article.date}
+					post={article.post.text}
+					description={article.description.text}
+					photo={article.photo.file.url}
+					category={article.categories[0].name} 
+				/>
+	        )}
+
+	    </ul>
 	</Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+	{
+        articles: allContentfulArticle(limit: 5) {
+		    edges {
+		      node {
+		        id
+						title
+						date
+						post: body {
+						  text: body
+						}
+						description: intro {
+						  text: intro
+						}
+						photo: mainImage {
+						  file {
+		            url
+		          }
+						}
+						categories {
+						  name
+						}
+		      }
+		    }
+		}
+    }
+`
