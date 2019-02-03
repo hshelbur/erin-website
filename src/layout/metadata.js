9e15ba2266df915eb8c4decacb49bbb9587387d1
Howard Shelburne <howard.shelburne@gmail.com>
2019-02-03 15:27:02 -0500

    added new og and twitter properties

--- src/layout/metadata.js
+++ src/layout/metadata.js
@@ -13,9 +13,12 @@ const Metadata = ({ name, title, keywords, description, image, url }) => (
     {title && <meta property="og:title" content={title} />}
     {description && <meta property="og:description" content={description} />}
     {image && <meta property="og:image" content={image} />}
+    {description && <meta property="og:title" content="article" />}
     {url && <meta property="og:url" content={url} />}
 
     {title && <meta name="twitter:title" content={title} />}
+    <meta name="twitter:side" content="@ErinTuringan" />
+    <meta name="twitter:creator" content="@ErinTuringan" />
     {description && <meta name="twitter:description" content={description} />}
     {image && <meta name="twitter:image" content={image} />}
     {image && <meta name="twitter:card" content={image} />}
