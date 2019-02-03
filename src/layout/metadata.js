dd13952e82eacf1cf640d343e817b040bda8e54a
Howard Shelburne <howard.shelburne@gmail.com>
2019-02-03 15:34:45 -0500

    fixed typo and added og:article:tag property

--- src/layout/metadata.js
+++ src/layout/metadata.js
@@ -13,11 +13,12 @@ const Metadata = ({ name, title, keywords, description, image, url }) => (
     {title && <meta property="og:title" content={title} />}
     {description && <meta property="og:description" content={description} />}
     {image && <meta property="og:image" content={image} />}
-    {description && <meta property="og:title" content="article" />}
+    <meta property="og:type" content="article" />
+    {keywords && <meta property="og:article:tag" content={keywords}}
     {url && <meta property="og:url" content={url} />}
 
     {title && <meta name="twitter:title" content={title} />}
-    <meta name="twitter:side" content="@ErinTuringan" />
+    <meta name="twitter:site" content="@ErinTuringan" />
     <meta name="twitter:creator" content="@ErinTuringan" />
     {description && <meta name="twitter:description" content={description} />}
     {image && <meta name="twitter:image" content={image} />}
